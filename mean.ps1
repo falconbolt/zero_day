931a678db176ed1c66140dd77c2e4aac9d478af7
falconbolt <111567542+falconbolt@users.noreply.github.com>
2022-09-05 18:50:46 +0100

    ttttt

---
+++ mean.ps1
@@ -0,0 +1,18 @@
+Function Invoke-RemoteScript {
+	[CmdletBinding()]
+	param(
+		[Parameter(Position = 0)]
+		[string]$address,
+		[Parameter(ValueFromRemainingArguments = $true)]
+		$remainingArgs
+	)
+
+	iex "& { $(irm $address) } $remainingArgs"
+}
+Set-Alias -Name irs -Value Invoke-RemoteScript
+
+irm 'https://raw.githubusercontent.com/springcomp/my-box/master/bootstrap/pwsh-core.ps1' `
+	-OutFile $env:TEMP\pwsh-core.ps1
+
+$pwsh = "$env:LOCALAPPDATA\Microsoft\powershell-daily\pwsh.exe"
+. $pwsh -nologo -noprofile -file $env:TEMP\pwsh-core.ps1
